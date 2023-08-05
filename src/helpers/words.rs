a86ee37d2bb5e681daace913c697062204e4f2aa
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2023-08-05 21:47:22 +0100

    Added error handling for failed loading of words

--- src/helpers/words.rs
+++ src/helpers/words.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use serde_json;
 
 use crate::helpers::random;
@@ -46,24 +48,38 @@ pub struct WordOptions {
 pub fn get_words(source_lang: Languages, target_lang: Languages, freq_limit: i64) -> WordOptions {
     let random_numbers = random::get_random_integers(freq_limit);
 
-    let word_list: serde_json::Value = load_words();
-
-    WordOptions {
-        source: word_list[random_numbers[0].to_string()][source_lang.to_str()]
-            .to_string()
-            .replace("\"", ""),
-        target_correct: word_list[random_numbers[0].to_string()][target_lang.to_str()]
-            .to_string()
-            .replace("\"", ""),
-        target_incorrect_1: word_list[random_numbers[1].to_string()][target_lang.to_str()]
-            .to_string()
-            .replace("\"", ""),
-        target_incorrect_2: word_list[random_numbers[2].to_string()][target_lang.to_str()]
-            .to_string()
-            .replace("\"", ""),
+    let word_list = load_words();
+    match word_list {
+        Ok(word_list) => WordOptions {
+            source: word_list[random_numbers[0].to_string()][source_lang.to_str()]
+                .to_string()
+                .replace("\"", ""),
+            target_correct: word_list[random_numbers[0].to_string()][target_lang.to_str()]
+                .to_string()
+                .replace("\"", ""),
+            target_incorrect_1: word_list[random_numbers[1].to_string()][target_lang.to_str()]
+                .to_string()
+                .replace("\"", ""),
+            target_incorrect_2: word_list[random_numbers[2].to_string()][target_lang.to_str()]
+                .to_string()
+                .replace("\"", ""),
+        },
+        Err(_) => WordOptions {
+            source: "Error".to_string(),
+            target_correct: "Error".to_string(),
+            target_incorrect_1: "Error".to_string(),
+            target_incorrect_2: "Error".to_string(),
+        },
     }
 }
 
-pub fn load_words() -> serde_json::Value {
-    serde_json::from_str(word_list::WORD_LIST).expect("Failed to parse JSON")
+pub fn load_words() -> Result<serde_json::Value, io::Error> {
+    let parsed_words = serde_json::from_str(word_list::WORD_LIST)?;
+    match parsed_words {
+        serde_json::Value::Object(_) => Ok(parsed_words),
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "WORD_LIST is not valid JSON",
+        )),
+    }
 }
